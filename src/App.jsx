e542dbfc54f823f5dd9aca9506e164b5f0cef448
shaikhaseeb-dev <shaikhaseeb004@gmail.com>
2026-03-28 13:18:52 +0530

    Removed Problem and CityCheck imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,24 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Problem from './components/Problem';
-import Solution from './components/Solution';
-import SolutionCards from './components/SolutionCards';
-import HowItWorks from './components/HowItWorks';
-import Trust from './components/Trust';
-import CityCheck from './components/CityCheck';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import WhatsAppFloat from './components/WhatsAppFloat';
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Solution from "./components/Solution";
+import SolutionCards from "./components/SolutionCards";
+import HowItWorks from "./components/HowItWorks";
+import Trust from "./components/Trust";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import WhatsAppFloat from "./components/WhatsAppFloat";
 
 export default function App() {
   return (
-    <div style={{ minHeight: '100vh' }}>
+    <div style={{ minHeight: "100vh" }}>
       <Navbar />
       <main>
         <Hero />
-        <Problem />
         <Solution />
         <SolutionCards />
         <HowItWorks />
         <Trust />
-        <CityCheck />
         <Contact />
       </main>
       <Footer />
